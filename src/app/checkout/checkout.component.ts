9714e08b4a1cbaa969a7f9c9ff5ae1a003af6a15
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-05-22 22:23:39 +0200

    Add print order item and order complete components, add order form to checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { ICart } from '../interfaces/ICart';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+	cartContent: ICart;
+	orderForm: FormGroup = this.fb.group({
+		firstName:[''],
+		lastName:[''],
+		address:[''],
+		postal:[''],
+		city:[''],
+		email:['']
+	});
 
-  constructor() { }
+	constructor(private cartService: CartService, private fb: FormBuilder) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.cartContent =	this.cartService.getCart();
+		this.cartService.thisMovie$.subscribe(addedMovie => {
+			this.cartContent = addedMovie;
+		});
+	}
 
 }
---
+++ src/app/print-order-item/print-order-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrintOrderItemComponent } from './print-order-item.component';
+
+describe('PrintOrderItemComponent', () => {
+  let component: PrintOrderItemComponent;
+  let fixture: ComponentFixture<PrintOrderItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PrintOrderItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrintOrderItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
