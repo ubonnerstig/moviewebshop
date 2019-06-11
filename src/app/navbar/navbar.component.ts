3ce2d827aa5216464ac074b63641e80b4023dbd0
Ulrica Bonnerstig <u.bonnerstig@gmail.com>
2019-06-11 02:15:57 +0200

    Add css for logo animation and remove search and cart from all sites except home

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { SearchService } from '../services/search.service';
 import { CartService } from '../services/cart.service';
-import { Data } from '@angular/router';
+import { Data, Router, NavigationEnd } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { ICategory } from '../interfaces/ICategory';
 
@@ -17,13 +17,13 @@ export class NavbarComponent implements OnInit {
 	cartQty: number;
 	cartQtyVisibility: boolean;
 	categories: ICategory[];
-	// categoryForm: FormGroup = this.fb.group({
-	// 	selectedCategory:['']
-	// });
+	miniLogo: boolean;
+	hideShowNav: boolean;
+	href: string;
 
 	selectedCategory = new FormControl('All');
 
-	constructor(private fb: FormBuilder, private searchService: SearchService, private cartService: CartService, private dataService: DataService) {}
+	constructor(private fb: FormBuilder, private searchService: SearchService, private cartService: CartService, private dataService: DataService, private router:Router) {}
 
 	ngOnInit() {
 		this.dataService.getCategories().subscribe(categories => {
@@ -37,6 +37,22 @@ export class NavbarComponent implements OnInit {
 		let qty = this.cartService.getCart();
 		this.cartQty = qty.totalQty;
 		this.checkCartQty(this.cartQty);
+
+		this.href = this.router.url;
+		// this.router.events.subscribe((val) => {
+		// 	console.log(val);
+		// });
+		this.router.events.forEach((event) => {
+			if(event instanceof NavigationEnd) {
+				console.log(event.url);
+				// console.log(event.url.replace("/",""));
+				if(event.url === "/"){
+					this.hideShowNav = false;
+				}else{
+					this.hideShowNav = true;
+				}
+			}
+		});
 	}
 
 	get category(){
@@ -62,5 +78,24 @@ export class NavbarComponent implements OnInit {
 
 	categorySearch(){
 		this.searchService.getCategory(this.category.value);
+	}
+
+	@HostListener('window:scroll', ['$event']) onScrollEvent($event){
+		// console.log($event.path[1].scrollY);
+		if ($event.path[1].scrollY >= 170){
+			this.miniLogo = true;
+			if(!this.hideShowNav){
+				document.getElementById("homeControls").style.flex = "80%";
+			}
+			// console.log(document.getElementById("mini_logo").style);
+			// console.log(document.getElementsByClassName("home_controls")[0].clientWidth);
+			// document.getElementsByClassName("home_controls")[0].clientWidth
+			// console.log(document.getElementById("homeControls").style);
+
+			// console.log($event.path[1].scrollY - 170);
+		}else if($event.path[1].scrollY <= 170) {
+			this.miniLogo = false;
+			// console.log(document.getElementById("mini_logo").style.width);
+		}
 	}
 }
